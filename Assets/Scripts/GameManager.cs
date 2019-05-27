6af282bc270b355305bf1615d7acc35267a6149c
ash-p98 <32458304+ash-p98@users.noreply.github.com>
2019-05-27 14:32:07 +0100

    Add files via upload

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+    private bool m_ReadyForInput;
+    public Player m_Player;
+
+    private void Update()
+    {
+        Vector2 moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        moveInput.Normalize();
+        if (moveInput.sqrMagnitude > 0.5)
+        {
+            if (m_ReadyForInput)
+            {
+                m_ReadyForInput = false;
+                m_Player.Move(moveInput);
+                //m_NextButton.SetActive(IsLevelComplete());
+            }
+        }
+        else
+        {
+            m_ReadyForInput = true;
+        }
+    }
+}
